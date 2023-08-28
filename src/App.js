7fa560c7ecd1aef27c5a3bb7fababb09e39f7a01
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-08-28 22:55:45 +0200

    adding some comments about default export and named export and import

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// 1. Create a component
+
+function App() {
+    return <h1>Bye there!</h1>;
+}
+
+export default App;
+
+// MODULE SYSTEM
+
+// in React project we are going to create a different file for each component
+// we also declare variales in different files and then share them between files
+
+// WITHOUT USING THE MODULE SYSTEM we cannot share variables between files
+
+// There is a import/export system in JS with many rules
+
+// there are two types of export files:
+// 1) default export
+// 2) named export
+
+// One file can only have one default export
